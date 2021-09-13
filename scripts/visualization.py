5673a6336e480ae503c60b044c544206610fa463
rashikcs <rashikatdasocalled@gmail.com>
2021-09-13 12:17:19 +0200

    updated barplots and documentation

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -22,20 +22,22 @@ def get_rows_cols_for_subplot(num_subplots:int)->tuple:
         odd:bool
     """
     
-    
-    nrows=ncols=0
-    odd=False
-    
-    if num_subplots%2==0 and num_subplots!=2:
-        nrows=ncols=round(num_subplots/2)
-    elif num_subplots==2:
-        nrows=1
-        ncols=2
-    else:
-        nrows=ncols=math.ceil(num_subplots/2)
-        odd=True
+    try:
+        nrows=ncols=0
+        odd=False
         
-    return nrows,ncols,odd
+        if num_subplots%2==0 and num_subplots!=2:
+            nrows=ncols=round(num_subplots/2)
+        elif num_subplots==2:
+            nrows=1
+            ncols=2
+        else:
+            nrows=ncols=math.ceil(num_subplots/2)
+            odd=True
+            
+        return nrows,ncols,odd
+    except Exception as error:
+        raise Exception('Caught this error: ' + repr(error))
 
 def get_axes_object_for_subplots(nrows:int,
                                  ncols:int,
@@ -51,38 +53,43 @@ def get_axes_object_for_subplots(nrows:int,
         ncols:int
         odd:bool
         fig:matplotlib.figure.Figure
-    
+        sharey:bool
+
     Returns:
         axes_list-> list
     """
-    
-    # Create nxn sub plots
-    gs = gridspec.GridSpec(nrows, ncols)
+    try:
 
-    axes_list = []
-    
-    for i in range(nrows):
-        for j in range(ncols):
-            
-            if odd:
+        # Create nxn sub plots
+        gs = gridspec.GridSpec(nrows, ncols)
+
+        axes_list = []
+        
+        for i in range(nrows):
+            for j in range(ncols):
                 
-                if j==1:
-                    odd=False
-                    continue
-                else:
-                    axes_list.append(fig.add_subplot(gs[i,:]))
+                if odd:
                     
-            else:
-                
-                if j==1:
-                    if sharey:
-                        axes_list.append(fig.add_subplot(gs[i,j], sharey = axes_list[-1]))
+                    if j==1:
+                        odd=False
+                        continue
+                    else:
+                        axes_list.append(fig.add_subplot(gs[i,:]))
+                        
+                else:
+                    
+                    if j==1:
+                        if sharey:
+                            axes_list.append(fig.add_subplot(gs[i,j], sharey = axes_list[-1]))
+                        else:
+                            axes_list.append(fig.add_subplot(gs[i,j]))
                     else:
                         axes_list.append(fig.add_subplot(gs[i,j]))
-                else:
-                    axes_list.append(fig.add_subplot(gs[i,j]))
-                    
-    return axes_list
+                        
+        return axes_list
+
+    except Exception as error:
+        raise Exception('Caught this error: ' + repr(error))
 
 def create_several_barplots_in_a_figure(data:pd.core.frame.DataFrame,
                                         x_axis_col_names:list,
@@ -106,6 +113,8 @@ def create_several_barplots_in_a_figure(data:pd.core.frame.DataFrame,
         num_subplots:int
         figure_title:str
         fontsize:int
+        sharey:bool                  -> wheather to sharen y-axis in same row
+        save_path:str
     
     """
     
@@ -163,15 +172,15 @@ def create_several_barplots_in_a_figure(data:pd.core.frame.DataFrame,
         raise Exception('Caught this error: ' + repr(error))
 
 def plot_line_graph(y:pd.core.frame.DataFrame,
-               x_label:str = 'Revenue',
-               y_label:str = 'Week Number',
-               trend_label:str = 'Weekly',
-               title:str = 'Title of the graph',
-               save_file:str='trend.png',
-               fig_dim_x:int=8,
-               fig_dim_y:int=4,
-               annnotate_values:bool=True,
-               save_path:str = None)->None:  
+                       x_label:str = 'Revenue',
+                       y_label:str = 'Week Number',
+                       trend_label:str = 'Weekly',
+                       title:str = 'Title of the graph',
+                       save_file:str='trend.png',
+                       fig_dim_x:int=8,
+                       fig_dim_y:int=4,
+                       annnotate_values:bool=True,
+                       save_path:str = None)->None:  
     """
     This function plots the data of given data frame to see the trend.
     
@@ -187,71 +196,67 @@ def plot_line_graph(y:pd.core.frame.DataFrame,
         annnotate_values:bool
         save_path:str
     """
-    
-    fig, ax = plt.subplots(figsize=(fig_dim_x, fig_dim_y))
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-    ax.plot(y, marker='.', linestyle='-', linewidth=0.5, label=trend_label)
-    ax.set_ylabel(x_label)
-    ax.set_xlabel(y_label)
-    ax.set_title(title)
-    ax.legend()
+    try:
 
-    if annnotate_values:
-        count = 0
-        for i,j in zip(y.index.values,y.values):
-            if count%4==0:
-                ax.annotate(str(j),xy=(i+0.05,j), fontsize=5)
-            count+=1
-    
-    if save_path:
-        plt.savefig( f"{save_path}/"+ save_file, dpi=300, bbox_inches = "tight")
+        fig, ax = plt.subplots(figsize=(fig_dim_x, fig_dim_y))
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.plot(y, marker='.', linestyle='-', linewidth=0.5, label=trend_label)
+        ax.set_ylabel(x_label)
+        ax.set_xlabel(y_label)
+        ax.set_title(title)
+        ax.legend()
 
-def add_value_labels(ax, spacing=5):
+        if annnotate_values:
+            count = 0
+            for i,j in zip(y.index.values,y.values):
+                if count%4==0:
+                    ax.annotate(str(j),xy=(i+0.05,j), fontsize=5)
+                count+=1
+        
+        if save_path:
+            plt.savefig( f"{save_path}/"+ save_file, dpi=300, bbox_inches = "tight")
+
+    except Exception as error:
+        raise Exception('Caught this error: ' + repr(error))
+
+def add_value_labels(ax, spacing=5)->None:
     """Add labels to the end of each bar in a bar chart.
 
     Arguments:
-        ax (matplotlib.axes.Axes): The matplotlib object containing the axes
-            of the plot to annotate.
-        spacing (int): The distance between the labels and the bars.
+        ax:matplotlib.axes.Axes   -> The matplotlib object containing the axes
+                                     of the plot to annotate.
+        spacing:int               -> The distance between the labels and the bars.
     """
+    try:
+        # For each bar: Place a label
+        for rect in ax.patches:
 
-    # For each bar: Place a label
-    for rect in ax.patches:
-        # Get X and Y placement of label from rect.
-        y_value = rect.get_height()
-        x_value = rect.get_x() + rect.get_width() / 2
+            y_value = rect.get_height()
+            x_value = rect.get_x() + rect.get_width() / 2
 
-        # Number of points between bar and label. Change to your liking.
-        space = spacing
-        # Vertical alignment for positive values
-        va = 'bottom'
+            space = spacing
+            # Vertical alignment for positive values
+            va = 'bottom'
 
-        # If value of bar is negative: Place label below bar
-        if y_value < 0:
-            # Invert space to place label below
-            space *= -1
-            # Vertically align label at top
-            va = 'top'
+            # If value of bar is negative: Place label below bar
+            if y_value < 0:
+                # Invert space to place label below
+                space *= -1
+                va = 'top'
 
-        # Use Y value as label and format number with one decimal place
-        label = "{:.1f}".format(y_value)
+            label = "{:.1f}".format(y_value)
+            ax.annotate(
+                label,                      
+                (x_value, y_value),         
+                xytext=(0, space),          
+                textcoords="offset points", 
+                ha='center',                
+                va=va,                      
+                fontsize=5
+                )                    
 
-        # Create annotation
-        ax.annotate(
-            label,                      # Use `label` as label
-            (x_value, y_value),         # Place label at end of the bar
-            xytext=(0, space),          # Vertically shift label by `space`
-            textcoords="offset points", # Interpret `xytext` as offset in points
-            ha='center',                # Horizontally center label
-            va=va,                      # Vertically align label differently for
-                                        # positive and negative values.
-            fontsize=5
-            )                     
-    # for p in ax.patches:
-    #     ax.annotate('{:.0f}'.format(p.get_height()), (p.get_x()+0.4, p.get_height()-p.get_height()+10),
-    #                 ha='center', va='bottom',
-    #                 color= 'white')
-
+    except Exception as error:
+        raise Exception('Caught this error: ' + repr(error))
 
 def plot_bargraph(df:pd.core.frame.DataFrame,
                   x_axis_column:str='product',
@@ -262,6 +267,7 @@ def plot_bargraph(df:pd.core.frame.DataFrame,
                   title:str='Tirle',
                   save_file:str='barplot.png',
                   conf_interval:bool=False,
+                  color_dict:dict = None,
                   save_path:str = None)->None:
     """
     This function plots the bargraph of the given two columns of the dataframe.
@@ -275,27 +281,31 @@ def plot_bargraph(df:pd.core.frame.DataFrame,
         y_label_name:str
         title:str
         save_file:str
+        color_dict:dict
         save_path:str
     """
-    
-    order = df.groupby([x_axis_column])[y_axis_column].mean().sort_values(ascending=False).index
-    ax = None
-    
-    if conf_interval:
-        ax = sns.barplot(x = x_axis_column, y = y_axis_column, hue=hue_column_name, data=df, order=order)
-    else:
-        ax = sns.barplot(x = x_axis_column, y = y_axis_column, hue=hue_column_name, data=df, order=order, ci=None)
 
-    ax.set(xlabel=x_label_name, ylabel=y_label_name)
-    ax.set_title(title)
-
-    # #plot bar values
-    add_value_labels(ax)
-
-    if save_path:
-        plt.savefig( f"{save_path}/"+ save_file, dpi=300, bbox_inches = "tight")
-
+    try:    
+        order = df.groupby([x_axis_column])[y_axis_column].mean().sort_values(ascending=False).index
+        ax = None
         
+        if conf_interval:
+            ax = sns.barplot(x = x_axis_column, y = y_axis_column, hue=hue_column_name, data=df, order=order, palette = color_dict)
+        else:
+            ax = sns.barplot(x = x_axis_column, y = y_axis_column, hue=hue_column_name, data=df, order=order, ci=None, palette = color_dict)
+
+        ax.set(xlabel=x_label_name, ylabel=y_label_name)
+        ax.set_title(title)
+
+        # #plot bar values
+        add_value_labels(ax)
+
+        if save_path:
+            plt.savefig( f"{save_path}/"+ save_file, dpi=300, bbox_inches = "tight")
+
+    except Exception as error:
+        raise Exception('Caught this error: ' + repr(error))
+
 def plot_highlighted_sphagetti_graph(df,
                                      unique_index_name:str='index',
                                      highlight_column:str='D',
@@ -313,10 +323,11 @@ def plot_highlighted_sphagetti_graph(df,
     
     Args:
         df:pd.core.frame.DataFrame
-        highlight_column:str  -> Column name to highlight
-        line_color:str
+        unique_index_name:str
+        highlight_column:str     -> Column name to highlight
         highlight_line_color:str
-        unique_index_name:list -> index column name of the dataframe
+        line_color:str
+        unique_index_name:list   -> index column name of the dataframe
         y_label:str     
         x_label:str     
         title:str       
@@ -324,43 +335,47 @@ def plot_highlighted_sphagetti_graph(df,
     Returns: 
         Updated dataframe-> consists of grouped columns and calculated result as a new column.
     
-    """        
+    """  
 
-    plt.style.use('seaborn-darkgrid')
+    try:
+        plt.style.use('seaborn-darkgrid')
 
-    # set figure size
-    plt.figure(figsize=(480/figsize_dpi, 480/figsize_dpi), dpi=figsize_dpi)
+        # set figure size
+        plt.figure(figsize=(480/figsize_dpi, 480/figsize_dpi), dpi=figsize_dpi)
 
-    # plot multiple lines
-    for column in df.drop(unique_index_name, axis=1):
-        plt.plot(df[unique_index_name], df[column], marker='', color= line_color, linewidth=1, alpha=0.4)
+        # plot multiple lines
+        for column in df.drop(unique_index_name, axis=1):
+            plt.plot(df[unique_index_name], df[column], marker='', color= line_color, linewidth=1, alpha=0.4)
 
-    # Now re do the interesting curve, but biger with distinct color
-    plt.plot(df[unique_index_name], df[highlight_column], marker='', color=highlight_line_color, linewidth=4, alpha=0.7)
 
-    # Change x axis limit
-    plt.xlim(df[unique_index_name].iloc[0]-1,df[unique_index_name].iloc[-1]+1)
+        plt.plot(df[unique_index_name], df[highlight_column], marker='', color=highlight_line_color, linewidth=4, alpha=0.7)
 
-    # Annotate the plot
-    num=0
-    for i in df.values[df.shape[0]-1][1:]:
-        num+=1
-        name=list(df)[num]
-        if name != highlight_column:
-            plt.text(26.2, i, name, horizontalalignment='left', size='small', color=line_color)
+        # Change x axis limit
+        plt.xlim(df[unique_index_name].iloc[0]-1,df[unique_index_name].iloc[-1]+1)
 
-    # And add a special annotation for the group we are interested in
-    plt.text(26.2, df[highlight_column].tail(1), '{}'.format(highlight_column), horizontalalignment='left', size='small', color='orange')
+        # Annotate the plot: hardcoded the text x-axis~26.2
+        num=0
+        for i in df.values[df.shape[0]-1][1:]:
+            num+=1
+            name=list(df)[num]
+            if name != highlight_column:
+                plt.text(26.2, i, name, horizontalalignment='left', size='small', color=line_color)
 
-    plt.title(title,\
-              loc='center', 
-              fontsize= 10, 
-              fontweight=0)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    
-    if save_path:
-        plt.savefig( f"{save_path}/"+ save_file, dpi=300, bbox_inches = "tight")
+        # And add a special annotation for the group we are interested in
+        plt.text(26.2, df[highlight_column].tail(1), '{}'.format(highlight_column), horizontalalignment='left', size='small', color='orange')
+
+        plt.title(title,\
+                  loc='center', 
+                  fontsize= 10, 
+                  fontweight=0)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
         
-    # Show the graph
-    plt.show()
+        if save_path:
+            plt.savefig( f"{save_path}/"+ save_file, dpi=300, bbox_inches = "tight")
+            
+        # Show the graph
+        plt.show()
+
+    except Exception as error:
+        raise Exception('Caught this error: ' + repr(error))
